3cc47cce5be688560ed5f2fcccec3b01c59da7a2
nikkie <takuyafjp+develop@gmail.com>
2019-12-22 14:39:09 +0900

    added number property method.

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ from typing import List
 class Card:
     def __init__(self, suit: str, number: int) -> None:
         self.suit = suit
-        self.number = number
+        self._number = number
+    
+    @property
+    def number(self):
+        return self._number if self._number < 11 else 10
 
 
 def calculate_score(cards: List['Card']) -> int:
--- test_main.py
+++ test_main.py
@@ -7,4 +7,4 @@ class CalculateScoreTestCase(unittest.TestCase):
     def test_sum_of_cards(self):
         cards = [m.Card('❤', 10), m.Card('♤', 11)]
         actual = m.calculate_score(cards)
-        self.assertEqual(actual, 21)
+        self.assertEqual(actual, 20)

df1b31ade8ac63eee572e7ef81c71d4156df8438
c-bata <contact@c-bata.link>
2019-12-22 14:11:07 +0900

    Add test case for sum of cards

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-def calculate_score(scores):
+from typing import List
+
+
+class Card:
+    def __init__(self, suit: str, number: int) -> None:
+        pass
+
+
+def calculate_score(scores: List['Card']) -> int:
     return sum(scores)
